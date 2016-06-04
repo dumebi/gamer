c06e2e231fda12124dfb853acb43033d4fbfc6ba
Jude <dikejude49@gmail.com>
2016-06-04 22:37:54 +0100

    new crypto function

--- storescripts/crypto.php
+++ storescripts/crypto.php
@@ -1,12 +1,21 @@
 <?php  
-function encryptIT($q){
-	$cryptkey = 'jjUB328DJBnbdkmsadJJK232JNkjk1319432jjjhbB';
-	$qEncoded = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($cryptkey), $q, MCRYPT_MODE_CBC, md5(md5($cryptkey))));
-	return ($qEncoded);
+function encrypt($pure_string) {
+    $dirty = array("+", "/", "=");
+    $clean = array("_PLUS_", "_SLASH_", "_EQUALS_");
+    $iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+    $_SESSION['iv'] = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+    $encrypted_string = mcrypt_encrypt(MCRYPT_BLOWFISH, $_SESSION['encryption-key'], utf8_encode($pure_string), MCRYPT_MODE_ECB, $_SESSION['iv']);
+    $encrypted_string = base64_encode($encrypted_string);
+    return str_replace($dirty, $clean, $encrypted_string);
 }
-function decryptIT($q){
-	$cryptkey = 'jjUB328DJBnbdkmsadJJK232JNkjk1319432jjjhbB';
-	$qDecoded = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($cryptkey), base64_decode($q), MCRYPT_MODE_CBC, md5(md5($cryptkey))), "\0");
-	return ($qDecoded);
+
+function decrypt($encrypted_string) { 
+    $dirty = array("+", "/", "=");
+    $clean = array("_PLUS_", "_SLASH_", "_EQUALS_");
+
+    $string = base64_decode(str_replace($clean, $dirty, $encrypted_string));
+
+    $decrypted_string = mcrypt_decrypt(MCRYPT_BLOWFISH, $_SESSION['encryption-key'],$string, MCRYPT_MODE_ECB, $_SESSION['iv']);
+    return $decrypted_string;
 }
 ?>
